02c557a8eb349f1b7abacb6900e17c7f8f098cd3
augusto <94504327+augustoba@users.noreply.github.com>
2023-02-04 22:04:06 -0300

    first commit

---
+++ Csharp-guia6-poo-ejercicio1/Csharp-guia6-poo-ejercicio1/entitiy/Libro.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_guia6_poo_ejercicio1.entitiy
+{
+    internal class Libro
+    {
+        private int Isbn;
+        private string Titulo;
+        private string Autor;
+        private int NumPag;
+
+        public Libro()
+        {
+        }
+
+        public Libro(int isbn, string titulo, string autor, int numPag)
+        {
+            Isbn = isbn;
+            Titulo = titulo;
+            Autor = autor;
+            NumPag = numPag;
+        }
+
+        public int getIsbn() 
+        { 
+           return this.Isbn;
+           }
+
+        public void setIsbn(int Isbn)
+        {
+            this.Isbn=Isbn;
+        }
+
+
+
+        public string getTitulo()
+        {
+            return this.Titulo;
+        }
+        public void setTitulo(string Titulo)
+        {
+            this.Titulo = Titulo;
+        }
+
+        public void setAutor(string Autor)
+        {
+            this.Autor = Autor;
+        }
+
+        public string getAutor()
+        {
+            return this.Autor;
+        }
+
+
+        public void setNumPag(int NumPag)
+        {
+            this.NumPag = NumPag;
+        }
+
+        public int getNumPag()
+        {
+            return this.NumPag;
+        }
+
+        public override string ToString()
+        {
+            return "Isbn: " + this.Isbn + ". Titulo: " + this.Titulo + ". Autor: " + this.Autor + ". Numero de paginas: " + this.NumPag;
+        }
+
+
+    }
+}
---
+++ Csharp-guia6-poo-ejercicio1/Csharp-guia6-poo-ejercicio1/service/LibroService.cs
@@ -0,0 +1,31 @@
+﻿using Csharp_guia6_poo_ejercicio1.entitiy;
+
+
+namespace Csharp_guia6_poo_ejercicio1.service
+{
+    internal class LibroService
+    {
+
+        Libro libro = new Libro();
+
+
+        public Libro crearLibro()
+        {
+
+            Console.WriteLine("ingrese el isbn del libro");
+            libro.setIsbn(int.Parse(Console.ReadLine()));
+            
+            Console.WriteLine("ingrese el titulo libro");
+            libro.setTitulo(Console.ReadLine());
+            
+            Console.WriteLine("ingrese el nombre del autor libro");
+            libro.setAutor(Console.ReadLine());
+
+            Console.WriteLine("ingrese el numero de paginas del libro");
+            libro.setNumPag(int.Parse(Console.ReadLine()));
+
+
+            return libro;
+        }
+    }
+}
